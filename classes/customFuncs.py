473ebea4d3e3434eb57558a7341e56ed0c6bb463
santaCoding <landreax100@gmail.com>
2020-05-25 16:33:44 +0300

    paid funcs 2.5/3

--- classes/customFuncs.py
+++ classes/customFuncs.py
@@ -8,37 +8,54 @@ def system_info():
 
 def addPaidOption(main_window, admin_access:bool, funcs:dict) -> str:
     '''
-    Принимает на вход ссылку на главное окно и статус админа
+    Принимает на вход ссылку на главное окно, статус админа и словарь функций
     Возвращает строку с результатом
     '''
-    def functionOn(event):
-        pass
-
-    def functionOff(event):
-        pass
-
-    if admin_access:
-        main_window.RIGHT_LABEL['text'] = 'Открыть платную функцию'
-        main_window.RIGHT_LABEL['pady'] = 40
+    def show_data():
         LEFT_FRAME_FUNCS = Frame(main_window.right_part)
         RIGHT_FRAME_FUNCS = Frame(main_window.right_part)
         LEFT_FRAME_FUNCS.place(x = 15, y = 100)
         RIGHT_FRAME_FUNCS.place(x = 290, y = 100)
-        LEFT_LABEL = Label(LEFT_FRAME_FUNCS, text='Список недоступных функций:')
-        RIGHT_LABEL = Label(RIGHT_FRAME_FUNCS, text='Список включенных функций:')
+        LEFT_LABEL = Label(LEFT_FRAME_FUNCS)
+        RIGHT_LABEL = Label(RIGHT_FRAME_FUNCS)
         LEFT_LABEL.pack()
         RIGHT_LABEL.pack()
         item = 0
+        falses = 0
+        trues = 0
         for function in funcs:
             if list(funcs.values())[item] == False:
+                falses += 1
                 func = Label(LEFT_FRAME_FUNCS, width = 25, text = function, bg='#485259', cursor='hand2', fg='white', pady=10, padx=10)
-                func.bind('<Button-1>', partial(functionOn))
+                func.bind('<Button-1>', partial(functionOn, function, LEFT_FRAME_FUNCS, RIGHT_FRAME_FUNCS))
                 func.pack(pady = 5)
             else:
+                trues += 1
                 func = Label(RIGHT_FRAME_FUNCS, width = 25, text = function, bg='#485259', cursor='hand2', fg='white', pady=10, padx=10)
-                func.bind('<Button-1>', partial(functionOn))
+                func.bind('<Button-1>', partial(functionOff, function, LEFT_FRAME_FUNCS, RIGHT_FRAME_FUNCS))
                 func.pack(pady = 5)
             item += 1
+        LEFT_LABEL['text'] = f'Список недоступных функций: [{falses}]'
+        RIGHT_LABEL['text'] = f'Список включенных функций: [{trues}]'
+
+    def functionOn(name_function, LEFT_FRAME_FUNCS, RIGHT_FRAME_FUNCS, event):
+        funcs[name_function] = True
+        LEFT_FRAME_FUNCS.destroy()
+        RIGHT_FRAME_FUNCS.destroy()
+        show_data()
+
+    def functionOff(name_function, LEFT_FRAME_FUNCS, RIGHT_FRAME_FUNCS, event):
+        funcs[name_function] = False
+        LEFT_FRAME_FUNCS.destroy()
+        RIGHT_FRAME_FUNCS.destroy()
+        show_data()
+
+    if admin_access:
+        main_window.RIGHT_LABEL['text'] = 'Открыть платную функцию'
+        main_window.RIGHT_LABEL['pady'] = 40
+        DONE_BUTTON = Button(main_window.right_frame, cursor='hand2', text='Выход', highlightbackground='#3b6ecc', highlightthickness=30, fg='white')
+        DONE_BUTTON.bind('<Button-1>', partial(done))
+        show_data()
         return 'Добавление функции (~ админ)'
     else:
         return 'У Вас нет доступа к функциям такого типа!\nВойдите под администратором'
